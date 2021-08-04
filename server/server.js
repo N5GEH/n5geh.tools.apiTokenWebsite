6f3db567d7d40cfd20a6cdc34620876de0f088b7
Stephan Wiemann <stephan.wiemann@tu-dresden.de>
2021-08-04 15:05:38 +0200

    Moved frontend into seperate folder, added Dockerfile.

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const HOST = process.env.HOST
 
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.use(
     auth({
@@ -72,7 +72,7 @@ app.get('/userinfo', async (req, res) => {
 //});
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
 
